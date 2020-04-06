10f463af8db710b4c3909efd35783e36adbfc57c
goobong <bongbonggg97@gmail.com>
2020-04-06 23:11:29 +0900

    Add text input by state manager: hooks

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 
 const TodoForm = ({ saveTodo }) => {
+  const [value, setValue] = useState('');
+
   return (
-    <form>
-      <TextField varient="outlined" placeholder="Add todo" margin="normal" />
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        saveTodo(value);
+      }}
+    >
+      <TextField 
+        varient="outlined" 
+        placeholder="Add todo" 
+        margin="normal" 
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+        value={value}
+        />
     </form>
   );
 };

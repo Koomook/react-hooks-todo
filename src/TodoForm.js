154eaa4688586666085a27f3df6acf59fdfa8a0e
goobong <bongbonggg97@gmail.com>
2020-04-07 13:31:41 +0900

    fea) Clear input

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -9,6 +9,8 @@ const TodoForm = ({ saveTodo }) => {
       onSubmit={(event) => {
         event.preventDefault();
         saveTodo(value);
+        
+        setValue('');
       }}
     >
       <TextField 

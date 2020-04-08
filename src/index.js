f692051ad37233640eb774122286f91c6ee3656f
goobong <bongbonggg97@gmail.com>
2020-04-08 23:16:12 +0900

    Delete unused useState

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import Typography from '@material-ui/core/Typography';
 import TodoForm from './TodoForm';

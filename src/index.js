f26cd6b710cd6cbea25dc4fc8aaaf6eb724b88af
goobong <bongbonggg97@gmail.com>
2020-04-08 21:39:07 +0900

    fea) Abstract useState

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Typography from '@material-ui/core/Typography';
-import './styles.css';
-
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
+import useTodoState from './useTodoState';
+import './styles.css';
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
+  const { todos, addTodo, deleteTodo} = useTodoState([]);
 
   return (
     <div className="App">
@@ -20,15 +20,11 @@ const App = () => {
           const trimmedText = todoText.trim();
 
           if (trimmedText.length > 0) {
-            setTodos([...todos, trimmedText]);
+            addTodo(trimmedText);
           }
         }}
       />
-      <TodoList todos={todos} deleteTodo={(todoIndex) =>{
-        const newTodos = todos.filter((_, index) => index !== todoIndex);
-
-        setTodos(newTodos);
-        }} />
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
     </div>
   );
 };

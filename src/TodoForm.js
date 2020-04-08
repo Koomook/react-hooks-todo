5b5d42113d1e11b5d23dfb203bae1cf9e0f77f0c
goobong <bongbonggg97@gmail.com>
2020-04-08 23:16:57 +0900

    Redesign: abstracting form input useState

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,27 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import useInputState from './useInputState';
 
 const TodoForm = ({ saveTodo }) => {
-  const [value, setValue] = useState('');
+  const { value, onChange, reset } = useInputState('');
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
+
         saveTodo(value);
-        
-        setValue('');
+        reset();
       }}
     >
-      <TextField 
-        varient="outlined" 
-        placeholder="Add todo" 
-        margin="normal" 
-        onChange={(event) => {
-          setValue(event.target.value);
-        }}
+      <TextField
+        variant="outlined"
+        placeholder="Add todo"
+        margin="normal"
+        onChange={onChange}
         value={value}
-        />
+      />
     </form>
   );
 };
